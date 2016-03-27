9e6797abfe00df6670a474d7b4c2150850488fb8
croeer <christian@roeer.info>
2016-03-27 12:25:03 +0200

    add verbosity option

--- parse.py
+++ parse.py
@@ -1,18 +1,23 @@
 import feedparser
 import argparse
 
-feedurl = "http://www.wdr.de/radio/home/nachrichten/feeds/verkehrsmeldungen.xml"
+parser = argparse.ArgumentParser(description="Parse wdr traffic rss feed")
+parser.add_argument("roads", help="comma-separated list of roads")
+parser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
+args = parser.parse_args()
 
+feedurl = "http://www.wdr.de/radio/home/nachrichten/feeds/verkehrsmeldungen.xml"
 f = feedparser.parse(feedurl)
 
-parser = argparse.ArgumentParser(description="Parse wdr traffic rss feed")
-parser.add_argument('roads', help='comma-separated list of roads')
-
-args = parser.parse_args()
+if (args.verbose):
+    print "Last update: " + f.updated
 
 for r in args.roads.split(","):
     road = r.upper()
-    #print "Road " + r.upper()
+    if (args.verbose):
+        print "Searching " + r.upper() + "..."
     for e in f.entries:
         if (e.title == road):
+            if (args.verbose):
+                print e
             print "(" + road + ") " + e.description
