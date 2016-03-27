4ef384c5ae8da0b027a47f7dff6b282695128ac7
croeer <christian@roeer.info>
2016-03-27 12:04:01 +0200

    implement cmd line parsing
    prints the message for each selected road

--- parse.py
+++ parse.py
@@ -4,3 +4,15 @@ import argparse
 feedurl = "http://www.wdr.de/radio/home/nachrichten/feeds/verkehrsmeldungen.xml"
 
 f = feedparser.parse(feedurl)
+
+parser = argparse.ArgumentParser(description="Parse wdr traffic rss feed")
+parser.add_argument('roads', help='comma-separated list of roads')
+
+args = parser.parse_args()
+
+for r in args.roads.split(","):
+    road = r.upper()
+    #print "Road " + r.upper()
+    for e in f.entries:
+        if (e.title == road):
+            print "(" + road + ") " + e.description
